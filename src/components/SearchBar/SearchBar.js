020c8aeb92c469295e99dcb386bb3b9ec98a0136
Andreas <andreas.hernas@gmail.com>
2021-04-20 13:47:46 +0200

    added search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,10 @@
 export default function SearchBar(){
     return(
         <>
+            <input
+                type="text"
+                placeholder="search product.."
+            />
         </>
     )
 }
