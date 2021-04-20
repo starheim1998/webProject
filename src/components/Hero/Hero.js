18e783a27e7f03e5eff4dcc612a72cf6f0828cfa
Andreas <andreas.hernas@gmail.com>
2021-04-20 13:51:20 +0200

    more templates added

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,8 @@
+
+export default function Hero(){
+    return (
+        <>
+            <h3>Insert hero here</h3>
+        </>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
+import Hero from "../../components/Hero/Hero";
 
 export default function Home(){
 
     return(
         <>
             <h2>Home</h2>
+            <Hero/>
         </>
     )
 }
