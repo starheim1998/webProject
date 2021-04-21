49e2f13e0faa161b16e46aa85ad7097344cac8e6
Andreas <andreas.hernas@gmail.com>
2021-04-21 09:36:32 +0200

    Added itemCard component

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,8 +1,23 @@
+import {Link} from "react-router-dom";
 
 
-export default function ItemCard(){
+export default function ItemCard( {product}){
+
+    // attributes of the product
+    const [id, img, name, price] = product;
+
+
     return(
-        <>
-        </>
+        <div className="card_container">
+            <Link to={`/item/${id}`}>
+                <div className="img_container">
+                    <img src={img} alt=".."/>
+                </div>
+                <div className="img_body">
+                    <h4>{name}</h4>
+                    <p>{price} kr</p>
+                </div>
+            </Link>
+        </div>
     )
 }
