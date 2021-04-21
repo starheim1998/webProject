8da8b501ed470bb09e7266f9635c81bfadefd183
Mathias <mvbend@stud.ntnu.no>
2021-04-21 14:01:09 +0200

    Home structure + Hero Structure

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import heroImage from "./../../img/hero-test.jpg"
+import "./Hero.css";
 
-export default function Hero(){
+export default function Hero() {
     return (
-        <>
-            <h3>Insert hero here</h3>
-        </>
+        <div className={"hero-container"}>
+            <img src={heroImage} alt={"hero image"}/>
+            <div className={"heroNOTphoto"}>
+                <h1>NEVER SEEN BEFORE SALE</h1>
+                <p>We are throwing a BIG sale this weekend for our brand new shoes!</p>
+                <div className={"hero-button"}>
+                    <button>SHOP NOW</button>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,22 @@
 import Hero from "../../components/Hero/Hero";
+import "./Home.css";
 
 export default function Home(){
 
     return(
-        <>
-            <h2>Home</h2>
+        <div>
             <Hero/>
-        </>
+
+            <div className={"home-container"}>
+                <h2>Trending right now...</h2>
+                <div className={"contentBox"}>
+                    <p>Items trending..</p>
+                </div>
+                <h2>For sale ..</h2>
+                <div className={"contentBox"}>
+                    <p>Items for sale..</p>
+                </div>
+            </div>
+        </div>
     )
 }
