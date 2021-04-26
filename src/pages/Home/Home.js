60272513db181a25b692081e8c60c0e214e05680
Mathias <mvbend@stud.ntnu.no>
2021-04-26 14:15:56 +0200

    Homescreen finito

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,6 +12,14 @@ import {useSelector} from "react-redux";
 export default function Home(){
     const itemState = useSelector((state) => state.itemReducer.items)
 
+
+    const founditems = [];
+    for (let i = 0; i < itemState.length; i++) {
+        if(itemState[i].id > 100){
+            founditems.push(itemState[i]);
+        }
+    }
+
     return(
         <div>
             <Hero/>
@@ -19,12 +27,12 @@ export default function Home(){
                 <h2>Trending right now...</h2>
                 <div className={"contentBox"}>
                     <p>Items trending!</p>
-                    <CardList cards={itemState.find(x => x.id > 100) }/>
+                    <CardList cards={founditems}/>
                 </div>
                 <h2>For sale ..</h2>
                 <div className={"contentBox"}>
                     <p>Items for sale!</p>
-                    <CardList cards={itemState.find(x => x.id >100)}/>
+                    <CardList cards={founditems}/>
                 </div>
             </div>
         </div>
