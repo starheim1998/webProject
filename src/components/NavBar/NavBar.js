9a05c13350dd514ff629812ed75927fae71b0232
Benjamin Engel <Hatlelid>
2021-04-27 11:11:48 +0200

    navbar clickable

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 
 import {useState} from "react";
 import './NavBar.css';
+import {Link} from "react-router-dom";
 export default function NavBar(){
     return(
         <Navbar_item>
@@ -23,10 +24,10 @@ function NavItem(props) {
     const[open, setOpen] = useState(false);
     return (
         <li className="nav-item">
-            <a className="male-button" onClick={() => setOpen(!open)}>
+            <a href={"#"} className="male-button" onClick={() => setOpen(!open)}>
                 Male
             </a>
-            <a className="female-button" onClick={() => setOpen(!open)}>
+            <a href={"#"} className="female-button" onClick={() => setOpen(!open)}>
                 Female
             </a>
             {open && props.children}
@@ -38,37 +39,37 @@ function NavItem(props) {
 function DropdownMenu() {
     function DropDownItem(props) {
         return (
-            <a onClick={console.log("hei")} className="menu-itemm">
+            <a onClick={console.log("hei")} className="menu-item">
                 {props.children}
             </a>
         );
     }
     return (
         <div className="dropdown">
-            <DropDownItem> Shoes </DropDownItem>
-            <DropDownItem> Bottom wear </DropDownItem>
-            <DropDownItem> Top wear </DropDownItem>
-            <DropDownItem> Sport </DropDownItem>
+            <DropDownItem>  <Link > Shoes </Link>  </DropDownItem>
+            <DropDownItem> <Link> Bottom wear </Link> </DropDownItem>
+            <DropDownItem> <Link> Top wear </Link> </DropDownItem>
+            <DropDownItem> <Link> Sport </Link> </DropDownItem>
 
-            <DropDownItem> Running </DropDownItem>
-            <DropDownItem> Jeans</DropDownItem>
-            <DropDownItem> T-shirt </DropDownItem>
-            <DropDownItem> Football </DropDownItem>
+            <DropDownItem> <Link> Running </Link> </DropDownItem>
+            <DropDownItem> <Link> Jeans </Link></DropDownItem>
+            <DropDownItem> <Link> T-shirt </Link> </DropDownItem>
+            <DropDownItem> <Link> Football </Link> </DropDownItem>
 
-            <DropDownItem> Lifestyle</DropDownItem>
-            <DropDownItem> Joggers </DropDownItem>
-            <DropDownItem> Sweater </DropDownItem>
-            <DropDownItem> Weightlifting </DropDownItem>
+            <DropDownItem> <Link> Lifestyle </Link></DropDownItem>
+            <DropDownItem> <Link> Joggers </Link> </DropDownItem>
+            <DropDownItem> <Link> Sweater </Link> </DropDownItem>
+            <DropDownItem> <Link> Weightlifting </Link> </DropDownItem>
 
-            <DropDownItem> Sneakers </DropDownItem>
-            <DropDownItem> Sweatpants </DropDownItem>
-            <DropDownItem> Singlet </DropDownItem>
-            <DropDownItem> Tennis </DropDownItem>
+            <DropDownItem> <Link> Sneakers </Link> </DropDownItem>
+            <DropDownItem> <Link> Sweatpants </Link> </DropDownItem>
+            <DropDownItem> <Link> Singlet </Link> </DropDownItem>
+            <DropDownItem> <Link> Tennis </Link> </DropDownItem>
 
-            <DropDownItem> Golf </DropDownItem>
-            <DropDownItem> Shorts </DropDownItem>
-            <DropDownItem> Cardigans </DropDownItem>
-            <DropDownItem> Basketball </DropDownItem>
+            <DropDownItem> <Link> Golf </Link> </DropDownItem>
+            <DropDownItem> <Link> Shorts </Link> </DropDownItem>
+            <DropDownItem> <Link> Cardigans </Link> </DropDownItem>
+            <DropDownItem> <Link> Basketball </Link> </DropDownItem>
         </div>
     );
 }
