f85c170d4abd57fa5483e3701fd0708f33eb208e
Mathias <mvbend@stud.ntnu.no>
2021-05-09 20:54:52 +0200

    frontpage small thing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import Hero from "../../components/Hero/Hero";
 import "./Home.css";
 import CardList from "../../components/CardList/CardList";
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getItems} from "../../store/actions/itemActions";
 
 /**
  * Home menu. We hardcoded the items for sale / trending categories as we did not see the implementation of it to
@@ -12,13 +14,24 @@ import {useSelector} from "react-redux";
 export default function Home(){
     const itemState = useSelector((state) => state.itemReducer.items);
 
-    // const [item, setItem]
+    useEffect(()=> {
+        getItems();
+    },[])
 
-    const getFrontPageItems = () => {
+    const getTrendingItems = () => {
         let list = [];
-        itemState.filter((item) => item.id > 100).
-            forEach((item) => list.push(item))
+        list.push(itemState[1])
+        list.push(itemState[2])
+        list.push(itemState[5])
         return list;
+    }
+
+    const getOnSaleItems = () => {
+        let saleList = [];
+        saleList.push(itemState[10])
+        saleList.push(itemState[11])
+        saleList.push(itemState[15])
+        return saleList;
     }
 
 
@@ -29,13 +42,13 @@ export default function Home(){
                 <h2>Trending right now...</h2>
                 <div className={"contentBox"}>
                     <p>Items trending!</p>
-                    <CardList cards={getFrontPageItems()}/>
+                    <CardList cards={getTrendingItems()}/>
                 </div>
                 <h2>For sale ..</h2>
-                {/*<div className={"contentBox"}>*/}
-                {/*    <p>Items for sale!</p>*/}
-                {/*    <CardList cards={founditems}/>*/}
-                {/*</div>*/}
+                <div className={"contentBox"}>
+                    <p>Items for sale!</p>
+                    <CardList cards={getOnSaleItems()}/>
+                </div>
             </div>
         </div>
     )
